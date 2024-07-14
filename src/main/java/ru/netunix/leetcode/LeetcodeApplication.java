24878b65dfc00225b72b4dd8d3ecf6300d15ece6
maria-shpatserman <sokunova@gmail.com>
2024-07-14 11:55:05 +0800

    First commit

---
+++ src/main/java/ru/netunix/leetcode/LeetcodeApplication.java
@@ -0,0 +1,13 @@
+package ru.netunix.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/netunix/leetcode/LeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netunix.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
